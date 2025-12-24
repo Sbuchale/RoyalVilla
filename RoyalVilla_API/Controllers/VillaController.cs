520da4a6b892e71d72ba8d6f215702ca1af62f27
admin <admin@DESKTOP-J7GDCD1>
2025-12-24 17:43:06 +0530

    Modify Villa Controller

--- RoyalVilla_API/Controllers/VillaController.cs
+++ RoyalVilla_API/Controllers/VillaController.cs
@@ -7,50 +7,18 @@ using System.Collections;
 
 namespace RoyalVilla_API.Controllers
 {
-    //[Route("api/[controller]")]
-    // [Route("/Villa")]
+
     [Route("api/Villa")]
     [ApiController]
     public class VillaController : ControllerBase
     {
-        #region FRomRout/fromQuery/fromheader
-
-
-        //[HttpGet]
-        //[Route("/villas")]
-        // [Route("/GetVilla")]
-
-        //[HttpGet]
-        //public string GetVillas()
-        //{
-        //    return "Get all Villas";
-        //}
-
-        //[HttpGet("{Id:int}")]
-        //public string GetVillaById(int id)
-        //{
-        //    return "Get Villa:" + id;
-        //}
-
-
-        //[HttpGet("{Id:int}/{name:string}")]
-        //[HttpGet("{id:int}/{name}")]
-        // public string GetVillaByIdAndName([FromRoute] int id, string name)
-        // public string GetVillaByIdAndName([FromQuery] int id, [FromQuery] string name)
-        //[HttpGet()]
-        //public string GetVillaByIdAndName([FromQuery] int id, [FromHeader] string name)
-        //{
-        //    return "Get Villa:" + id + ":" + name;
-        //}
-
-        #endregion
 
         private readonly ApplicationDbContext _db;
         public VillaController(ApplicationDbContext db)
         {
             _db = db;
         }
-
+         
         [HttpGet]
         public IEnumerable<Villa> GetVillas()
         {
