1fc407448708450f09a9af5242ae884e01dd4ab1
admin <admin@DESKTOP-J7GDCD1>
2025-12-24 17:37:34 +0530

    Add project files.

---
+++ RoyalVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RoyalVilla_API.Data;
+using RoyalVilla_API.Models;
+using System.Collections;
+
+namespace RoyalVilla_API.Controllers
+{
+    //[Route("api/[controller]")]
+    // [Route("/Villa")]
+    [Route("api/Villa")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        #region FRomRout/fromQuery/fromheader
+
+
+        //[HttpGet]
+        //[Route("/villas")]
+        // [Route("/GetVilla")]
+
+        //[HttpGet]
+        //public string GetVillas()
+        //{
+        //    return "Get all Villas";
+        //}
+
+        //[HttpGet("{Id:int}")]
+        //public string GetVillaById(int id)
+        //{
+        //    return "Get Villa:" + id;
+        //}
+
+
+        //[HttpGet("{Id:int}/{name:string}")]
+        //[HttpGet("{id:int}/{name}")]
+        // public string GetVillaByIdAndName([FromRoute] int id, string name)
+        // public string GetVillaByIdAndName([FromQuery] int id, [FromQuery] string name)
+        //[HttpGet()]
+        //public string GetVillaByIdAndName([FromQuery] int id, [FromHeader] string name)
+        //{
+        //    return "Get Villa:" + id + ":" + name;
+        //}
+
+        #endregion
+
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return _db.Villa.ToList();
+        }
+
+        [HttpGet("{Id:int}")]
+        public string GetVillaById(int id)
+        {
+            return "Get Villa:" + id;
+        }
+    }
+}
---
+++ RoyalVilla_API/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using RoyalVilla_API.Data;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); 
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+//call below SeedDataAsync method here
+
+await SeedDataAsync(app);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+//below function fo Automatically apply any pending migration
+static async Task SeedDataAsync(WebApplication app)
+{
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    await context.Database.MigrateAsync();
+}
+
